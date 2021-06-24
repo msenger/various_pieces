14c7c7c81e084a0d3df0941e2deb3745b1caa928
Martin Senger <martin.senger@gmail.com>
2021-06-24 09:49:38 +0200

    consumer key added

--- geocaching/AdventureLabs/smart-to-labid.pl
+++ geocaching/AdventureLabs/smart-to-labid.pl
@@ -63,6 +63,7 @@ use LWP::UserAgent;
 use open OUT => ':utf8';
 my $ua = LWP::UserAgent->new;
 $ua->agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
+$ua->default_header ('X-Consumer-Key' => "A01A9CA1-29E0-46BD-A270-9D894A527B91");
 
 # ----------------------------------------------------------------
 # Get a serie by its SmartLink (e.g. https://labs.geocaching.com/goto/stredokluky)
