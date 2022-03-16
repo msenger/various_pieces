e18451de22ecfd4311894f169b7e67d588b8a2c3
Martin Senger <martin.senger@gmail.com>
2022-03-16 15:03:11 +0100

    emailing corrected

--- geocaching/AdventureLabs/notify.bat
+++ geocaching/AdventureLabs/notify.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 set MY_ERR=err.tmp
 set MY_STD=std.tmp
-perl test1.pl -stderr -stdout 2> "%MY_ERR%" 1> "%MY_STD%"
+perl find-new-labs.pl -nget -q 2> "%MY_ERR%" 1> "%MY_STD%"
 perl wrap-and-email.pl < "%MY_STD%"
 perl wrap-and-email.pl -admin < "%MY_ERR%"
